957d7652045f81bb1543fde633fcfeb5433cc87a
Lê Thanh Sơn <ltsson1111@gmail.com>
2021-11-11 22:24:09 +0700

    ThanhSon

---
+++ R4Clothes.Server/R4Clothes.Server/Pages/Index.razor.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace R4Clothes.Server.Pages
+{
+    public partial class Index
+    {
+        List<SanPham> sp = null;
+        public class SanPham
+        {
+            [Key]
+            public int Masanpham { get; set; }
+
+            [ForeignKey("QuanTris")]
+            public int Maquantri { get; set; }
+
+            [StringLength(250)]
+            [Required(ErrorMessage = "Bạn cần nhập tên.")]
+            [Display(Name = "Tên sản phẩm")]
+            public string Tensanpham { get; set; }
+
+            [Display(Name = "Mã loại")]
+            [Required(ErrorMessage = "Bạn cần chọn mã loại."), Range(1, int.MaxValue, ErrorMessage = "Bạn cần chọn mã loại.")]
+            [ForeignKey("LoaiSanPhams")]
+            public int Maloai { get; set; }
+
+            public int Soluong { get; set; }
+
+            [Required(ErrorMessage = "Bạn cần nhập giá."), Range(0, double.MaxValue, ErrorMessage = "Bạn cần nhập giá.")]
+            [Display(Name = "Giá")]
+            public double Gia { get; set; }
+
+            [StringLength(100)]
+            [Display(Name = "Hình")]
+            public string Hinh { get; set; }
+
+            public int Soluotxem { get; set; }
+
+            public DateTime Ngaynhap { get; set; }
+
+            public int? Giamgia { get; set; }
+
+            [StringLength(250)]
+            [Display(Name = "Mô tả")]
+            public string Mota { get; set; }
+
+            [Display(Name = "Trạng thái")]
+            public bool Trangthai { get; set; }
+
+            public bool Dacbiet { get; set; }
+
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var ss = await client.GetStringAsync("https://localhost:44356/api/sanphams/dssanpham");
+                Console.WriteLine(ss);
+                sp = JsonConvert.DeserializeObject<List<SanPham>>(ss);
+            }
+        }
+
+    }
+}
+
